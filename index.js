e6ba574d6e98fee27ab244bfa3fd37b9a6e6d3fc
Pastakun <130649332+Pastakun@users.noreply.github.com>
2025-04-27 10:22:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,149 +25,158 @@ axios
       usercode = response.data;
     });
   });
-const ws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
-ws.on("message", (data) => {
-  const buffer = Buffer.from(data);
-  const jsonString = buffer.toString("utf8");
-  const jsonObject = JSON.parse(jsonString);
-  if (jsonObject.op === 10) {
-    const interval = jsonObject.d.heartbeat_interval;
-    heartbeatInterval = setInterval(() => {
-      ws.send(JSON.stringify({ op: 1, d: null }));
+let ws;
+let heartbeatInterval;
+function connect(){
+  ws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
+  ws.on("message", (data) => {
+    const buffer = Buffer.from(data);
+    const jsonString = buffer.toString("utf8");
+    const jsonObject = JSON.parse(jsonString);
+    if (jsonObject.op === 10) {
+      const interval = jsonObject.d.heartbeat_interval;
+      heartbeatInterval = setInterval(() => {
+        ws.send(JSON.stringify({ op: 1, d: null }));
+        ws.send(
+          JSON.stringify({
+            op: 3,
+            d: {
+              since: null,
+              activities: [
+                {
+                  name: `test`,
+                  type: 0,
+                },
+              ],
+              status: "online",
+              afk: false,
+            },
+          })
+        );
+      }, interval);
+  
       ws.send(
         JSON.stringify({
-          op: 3,
+          op: 2,
           d: {
-            since: null,
-            activities: [
-              {
-                name: `test`,
-                type: 0,
-              },
-            ],
-            status: "online",
-            afk: false,
+            token: process.env.token,
+            intents: 131071,
+            properties: {
+              $os: process.platform,
+              $browser: "Pasta Bot",
+              $device: "Pasta Bot",
+            },
           },
         })
       );
-    }, interval);
-
-    ws.send(
-      JSON.stringify({
-        op: 2,
-        d: {
-          token: process.env.token,
-          intents: 131071,
-          properties: {
-            $os: process.platform,
-            $browser: "Pasta Bot",
-            $device: "Pasta Bot",
-          },
-        },
-      })
-    );
-  }
-  if (jsonObject.t === "MESSAGE_CREATE") {
-    const data = jsonObject.d;
-    if (data.author.id !== "1365619295911936141") {
-      for (const userId in usercode) {
-        let send = true;
-        const result = vm.runInNewContext(
-          usercode[userId].message,
+    }
+    if (jsonObject.t === "MESSAGE_CREATE") {
+      const data = jsonObject.d;
+      if (data.author.id !== "1365619295911936141") {
+        for (const userId in usercode) {
+          let send = true;
+          const result = vm.runInNewContext(
+            usercode[userId].message,
+            {
+              content: data.content,
+              channel_id: data.channel_id,
+              message: (channel, embed) => {
+                if (send) {
+                  axios.post(
+                    `https://discord.com/api/v10/channels/${channel}/messages`,
+                    { embeds: [embed] },
+                    {
+                      headers: {
+                        Authorization: `Bot ${process.env.token}`,
+                        "Content-Type": "application/json",
+                      },
+                    }
+                  );
+                  send = false;
+                }
+              },
+            },
+            { timeout: 100 }
+          );
+        }
+      }
+    }
+    if (jsonObject.t === "INTERACTION_CREATE") {
+      const data = jsonObject.d;
+      const userId = data.member.user.id;
+      if (data.data.custom_id?.startsWith("code_")) {
+        const event = data.data.custom_id.split("_")[1];
+        if (!(userId in usercode)) {
+          usercode[userId] = { message: "" };
+        }
+        const code = data.data.components[0].components[0].value;
+        usercode[userId][event] = code;
+        axios.post(
+          `https://discord.com/api/v10/interactions/${data.id}/${data.token}/callback`,
           {
-            content: data.content,
-            channel_id: data.channel_id,
-            message: (channel, embed) => {
-              if (send) {
-                axios.post(
-                  `https://discord.com/api/v10/channels/${channel}/messages`,
-                  { embeds: [embed] },
-                  {
-                    headers: {
-                      Authorization: `Bot ${process.env.token}`,
-                      "Content-Type": "application/json",
-                    },
-                  }
-                );
-                send = false;
-              }
+            type: 4,
+            data: {
+              embeds: [
+                {
+                  title: event,
+                  description: code,
+                  color: 0x999999,
+                },
+              ],
             },
           },
-          { timeout: 100 }
+          {
+            headers: {
+              Authorization: `Bot ${process.env.token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+      }
+      if (data.data.name === "code") {
+        axios.post(
+          `https://discord.com/api/v10/interactions/${data.id}/${data.token}/callback`,
+          {
+            type: 9,
+            data: {
+              title: "コードを入力",
+              custom_id: `code_${data.data.options[0].value}`,
+              components: [
+                {
+                  type: 1,
+                  components: [
+                    {
+                      type: 4,
+                      custom_id: "text",
+                      label: "Code",
+                      style: 2,
+                      min_length: 0,
+                      max_length: 1000,
+                      placeholder: "",
+                      required: false,
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          {
+            headers: {
+              Authorization: `Bot ${process.env.token}`,
+              "Content-Type": "application/json",
+            },
+          }
         );
       }
     }
-  }
-  if (jsonObject.t === "INTERACTION_CREATE") {
-    const data = jsonObject.d;
-    const userId = data.member.user.id;
-    if (data.data.custom_id?.startsWith("code_")) {
-      const event = data.data.custom_id.split("_")[1];
-      if (!(userId in usercode)) {
-        usercode[userId] = { message: "" };
-      }
-      const code = data.data.components[0].components[0].value;
-      usercode[userId][event] = code;
-      axios.post(
-        `https://discord.com/api/v10/interactions/${data.id}/${data.token}/callback`,
-        {
-          type: 4,
-          data: {
-            embeds: [
-              {
-                title: event,
-                description: code,
-                color: 0x999999,
-              },
-            ],
-          },
-        },
-        {
-          headers: {
-            Authorization: `Bot ${process.env.token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-    if (data.data.name === "code") {
-      axios.post(
-        `https://discord.com/api/v10/interactions/${data.id}/${data.token}/callback`,
-        {
-          type: 9,
-          data: {
-            title: "コードを入力",
-            custom_id: `code_${data.data.options[0].value}`,
-            components: [
-              {
-                type: 1,
-                components: [
-                  {
-                    type: 4,
-                    custom_id: "text",
-                    label: "Code",
-                    style: 2,
-                    min_length: 0,
-                    max_length: 1000,
-                    placeholder: "",
-                    required: false,
-                  },
-                ],
-              },
-            ],
-          },
-        },
-        {
-          headers: {
-            Authorization: `Bot ${process.env.token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-  }
-});
-setInterval(async function () {
+  });
+  ws.on("close", (data) => {
+    clearInterval(heartbeatInterval);
+    setTimeout(connect, 5000);
+  });
+}
+connect();
+setInterval(async () => {
   const jsonString = JSON.stringify(usercode);
 
   const stream = Readable.from([jsonString]);
